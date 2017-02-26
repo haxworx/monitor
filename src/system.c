2bedba7025e95a784fd59c73447e80caa5b512c8
Al Poole <netstar@gmail.com>
2017-02-26 17:10:40 +0000

    cpu probe

--- src/system.c
+++ src/system.c
@@ -9,23 +9,43 @@
 # include <sys/param.h>
 # include <sys/sysctl.h>
 #endif
+#include <stdlib.h>
+#include <string.h>
 
 int
 system_cpu_count(void)
 {
-	int generic = 4;
-#if defined(__WIN32__)
-	const char *cpu_count = getenv("NUMBER_OF_PROCESSORS");
-	if (!cpu_count) return genetic;
-	else return atoi(cpu_count);
-#elif defined(__FreeBSD__) || defined(__DragonFly_) || defined(__OpenBSD__)
+	int generic = 1;
 	int cores = 0;
+	const char *cpu_count = getenv("NUMBER_OF_PROCESSORS");
+	if (cpu_count) {
+		cores = atoi(cpu_count);
+		return cores;
+	}
+#if defined(__FreeBSD__) || defined(__DragonFly_) || defined(__OpenBSD__)
 	size_t len;
 	int mib[2] = { CTL_HW, HW_NCPU };
 
 	len = sizeof(cores);
 	if (sysctl(mib, 2, &cores, &len, NULL, 0) == -1) return generic;
 	return cores;
+#elif defined(__linux__)
+	char buf[4096];
+	FILE *f = fopen("/proc/stat", "r");
+	if (!f) return generic;
+	int line = 0;
+	while (fgets(buf, sizeof(buf), f)) {
+		if (line) {
+			char *tok = strtok(buf, " ");
+			if (!strncmp(tok, "cpu", 3))
+				++cores;
+			else break;
+		}	
+		line++;
+	}
+	fclose(f);
+
+	return cores || generic;
 #else
 	return generic;
 #endif
